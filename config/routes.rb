15c4b6a937b400db789d9bc0f7bc1c42d52036f6
John Serafin <j-serafin@northwestern.edu>
2019-02-15 12:57:00 -0600

    Remove redundant user resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :grants do
     resources :questions
   end
-  resources :users
+
   resources :organizations
   resources :users, only: %i[index edit update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
