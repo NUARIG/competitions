9056d2dc5cf85e29cb63b431327b19223c3c34bb
mattbaumann1 <mattbaumann1@users.noreply.github.com>
2019-06-24 15:13:44 -0500

    Update criterion.rb

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -1,7 +1,7 @@
 class Criterion < ApplicationRecord
   belongs_to :grant,            inverse_of: :criteria
   has_many   :criteria_reviews
-  # TODOL: will we need this?
+  # TODO: will we need this?
   # has_many   :reviews,          through: :criterion_reviews
 
   validates_presence_of :name
