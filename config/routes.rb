ebfeea976b6a95989a04ffec6d74316f11261701
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-07-01 13:39:43 -0500

    WIP: change routes and credentials. #93 #99

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,15 @@ Rails.application.routes.draw do
   resources :reviews
 
   resources :users, only: %i[show index edit update]
+
+  # opt-in saml_authenticatable
+  devise_scope :user do
+    scope "users", controller: 'saml_sessions' do
+      get :new, path: "sign_in", as: :new_user_sso_session
+      post :create, path: "saml/auth", as: :user_sso_session
+      get :destroy, path: "sign_out", as: :destroy_user_sso_session
+      get :metadata, path: "saml/metadata", as: :metadata_user_sso_session
+      match :idp_sign_out, path: "saml/idp_sign_out", via: [:get, :post]
+    end
+  end
 end
