fedc27b7aaf68c4259782f82ed9272fea2976dc8
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-07-15 16:38:03 -0500

    WIP re-add validations for names. #93 #99

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,8 @@ class User < ApplicationRecord
   # validates :organization, presence: true
   validates :organization_role, presence: true
   validates :email, presence: true, uniqueness: true
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   scope :with_organization, -> { includes :organization }
 
