6d929ea5547274788f286bb1bc67ce21f68bcf06
jseraf <j-serafin@northwestern.edu>
2019-09-11 16:50:36 -0500

    Fix extenstion on grant_creator_request mailer locale file

--- app/mailers/grant_creator_request_review_mailer.rb
+++ app/mailers/grant_creator_request_review_mailer.rb
@@ -2,11 +2,11 @@ class GrantCreatorRequestReviewMailer < ApplicationMailer
   before_action :set_attributes
 
   def approved
-    mail(to: @request.requester.email, subject: I18n.t('mailers.grant_creator_request_reviews.approved.subject'))
+    mail(to: @request.requester.email, subject: I18n.t('mailers.grant_creator_request_review.approved.subject'))
   end
 
   def rejected
-    mail(to: @request.requester.email, subject: I18n.t('mailers.grant_creator_request_reviews.rejected.subject'))
+    mail(to: @request.requester.email, subject: I18n.t('mailers.grant_creator_request_review.rejected.subject'))
   end
 
   def set_attributes
