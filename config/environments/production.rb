67210dbedb3ce3219982bda47dc4e059a85022b0
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-09-13 14:46:58 -0500

    remove prod env configs. #191

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,10 +41,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-
-  # set this in storage.yml and credentials file
-  # config.active_storage.service = :local
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -73,14 +70,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.default_url_options = Rails.application.credentials.dig(Rails.env.to_sym, :default_url_options)
-
-  config.subdomain = Rails.application.credentials.dig(Rails.env.to_sym, :subdomain)
-  config.app_domain = Rails.application.credentials.dig(Rails.env.to_sym, :app_domain)
-  config.action_mailer.delivery_method = Rails.application.credentials.dig(Rails.env.to_sym, :delivery_method).to_sym
-  config.action_mailer.perform_deliveries = Rails.application.credentials.dig(Rails.env.to_sym, :perform_deliveries)
-  config.action_mailer.smtp_settings = Rails.application.credentials.dig(Rails.env.to_sym, :smtp_settings)
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
