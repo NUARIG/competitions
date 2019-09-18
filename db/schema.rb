62af4cbcd1faf7d3ec5c2376c8137d17d9b5823d
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-09-18 15:51:36 -0500

    WIP add session_index to user. #167

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_221838) do
+ActiveRecord::Schema.define(version: 2019_09_18_194132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -419,6 +419,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_221838) do
     t.inet "last_sign_in_ip"
     t.boolean "grant_creator", default: false, null: false
     t.string "upn", default: "", null: false
+    t.string "session_index", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
