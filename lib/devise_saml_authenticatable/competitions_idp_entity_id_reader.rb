5f23bdbf750d8cf137702a2643ca79be47ad6cfc
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-09-23 16:27:33 -0500

    WIP: Changes to the idp_entity_id_reader. #167

--- lib/devise_saml_authenticatable/competitions_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/competitions_idp_entity_id_reader.rb
@@ -8,11 +8,44 @@ module DeviseSamlAuthenticatable
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuer
       elsif params[:SAMLResponse]
-        OneLogin::RubySaml::Response.new(
-          params[:SAMLResponse],
-          settings: Devise.saml_config,
-          allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
-        ).issuers.first
+        # Rails.logger.info("I am in the first elsif")
+        # r = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
+        # Rails.logger.info("Message: #{r.document.inspect}~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        # if REXML::XPath.match(r, "/p:LogoutResponse/a:Issuer", { "p" => PROTOCOL, "a" => ASSERTION }).any?
+        #   OneLogin::RubySaml::SloLogoutresponse.new(
+        #     params[:SAMLRequest],
+        #     settings: Devise.saml_config,
+        #     allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+        #   ).issuer
+        # else
+        #   OneLogin::RubySaml::Response.new(
+        #     params[:SAMLResponse],
+        #     settings: Devise.saml_config,
+        #     allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+        #   ).issuers.first
+        # end
+        begin
+          OneLogin::RubySaml::Response.new(
+            params[:SAMLResponse],
+            settings: Devise.saml_config,
+            allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+          ).issuers.first
+        rescue
+          byebug
+
+          OneLogin::RubySaml::SloLogoutresponse.new(
+            params[:SAMLRequest],
+            settings: Devise.saml_config,
+            allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+          ).issuer
+          Devise.sign_out_and_redirect(current_user)
+
+          # redirect_to Devise.saml_sign_out_success_url
+
+
+        end
+
+
       end
     end
   end
