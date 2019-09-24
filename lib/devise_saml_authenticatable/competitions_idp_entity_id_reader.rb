d2faa11cadc483f74aa28d9cc47f27b4a315bf8c
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-09-24 10:19:52 -0500

    WIP: Change reader back with loggers. #167

--- lib/devise_saml_authenticatable/competitions_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/competitions_idp_entity_id_reader.rb
@@ -2,18 +2,69 @@ module DeviseSamlAuthenticatable
   class CompetitionsIdpEntityIdReader
     def self.entity_id(params)
       if params[:SAMLRequest]
+        Rails.logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        Rails.logger.info("I am in the first if")
         OneLogin::RubySaml::SloLogoutrequest.new(
           params[:SAMLRequest],
           settings: Devise.saml_config,
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuer
       elsif params[:SAMLResponse]
-        OneLogin::RubySaml::Response.new(
-          params[:SAMLResponse],
-          settings: Devise.saml_config,
-          allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
-        ).issuers.first
+        Rails.logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        Rails.logger.info("I am in the first elsif")
+        # r = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
+        # Rails.logger.info("Message: #{r.document.inspect}~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        # if REXML::XPath.match(r, "/p:LogoutResponse/a:Issuer", { "p" => PROTOCOL, "a" => ASSERTION }).any?
+        #   OneLogin::RubySaml::SloLogoutresponse.new(
+        #     params[:SAMLRequest],
+        #     settings: Devise.saml_config,
+        #     allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+        #   ).issuer
+        # else
+        #   OneLogin::RubySaml::Response.new(
+        #     params[:SAMLResponse],
+        #     settings: Devise.saml_config,
+        #     allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+        #   ).issuers.first
+        # end
+        begin
+          Rails.logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+          Rails.logger.info("I am in the first begin")
+          OneLogin::RubySaml::Response.new(
+            params[:SAMLResponse],
+            settings: Devise.saml_config,
+            allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+          ).issuers.first
+        rescue
+          Rails.logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+          Rails.logger.info("I am in the first rescue")
+          OneLogin::RubySaml::SloLogoutresponse.new(
+            params[:SAMLRequest],
+            settings: Devise.saml_config,
+            allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+          ).issuer
+        end
       end
     end
   end
 end
+
+# module DeviseSamlAuthenticatable
+#   class CompetitionsIdpEntityIdReader
+#     def self.entity_id(params)
+#       if params[:SAMLRequest]
+#         OneLogin::RubySaml::SloLogoutrequest.new(
+#           params[:SAMLRequest],
+#           settings: Devise.saml_config,
+#           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+#         ).issuer
+#       elsif params[:SAMLResponse]
+#         OneLogin::RubySaml::Response.new(
+#           params[:SAMLResponse],
+#           settings: Devise.saml_config,
+#           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
+#         ).issuers.first
+#       end
+#     end
+#   end
+# end
