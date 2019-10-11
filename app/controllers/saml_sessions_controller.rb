2e85e6830ed9e4a6d8985c68479636b95763cac4
jseraf <j-serafin@northwestern.edu>
2019-10-11 17:11:12 -0500

    WIP: move session set after create, remove unused method

--- app/controllers/saml_sessions_controller.rb
+++ app/controllers/saml_sessions_controller.rb
@@ -1,16 +1,12 @@
 # require "ruby-saml"
 
 class SamlSessionsController < Devise::SamlSessionsController
-  prepend_before_action :set_user_session_index, only: :create
+  after_action :set_user_session_index, only: :create
   skip_before_action :verify_authenticity_token, raise: false
   prepend_before_action :store_info_for_sp_initiated_logout, only: :destroy
   prepend_before_action :get_user_session_index, only: :destroy
 
   protected
-
-  def set_session_index
-    session[:session_index] = current_user
-  end
 
   def set_user_session_index
     session[:saml_session_index] = current_user.session_index if user_signed_in?
