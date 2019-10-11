ab127e143d91fa5e592e6b6d4f26dd5f94419108
jseraf <j-serafin@northwestern.edu>
2019-10-11 16:59:09 -0500

    Override Devise session controller to store session_index to session

---
+++ app/controllers/saml_sessions_controller.rb
@@ -0,0 +1,22 @@
+# require "ruby-saml"
+
+class SamlSessionsController < Devise::SamlSessionsController
+  prepend_before_action :set_user_session_index, only: :create
+  skip_before_action :verify_authenticity_token, raise: false
+  prepend_before_action :store_info_for_sp_initiated_logout, only: :destroy
+  prepend_before_action :get_user_session_index, only: :destroy
+
+  protected
+
+  def set_session_index
+    session[:session_index] = current_user
+  end
+
+  def set_user_session_index
+    session[:saml_session_index] = current_user.session_index if user_signed_in?
+  end
+
+  def get_user_session_index
+    current_user.session_index = session[:saml_session_index]
+  end
+end
