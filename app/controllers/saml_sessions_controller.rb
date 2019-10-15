539d3621254db7431cae3ae0ab5f2545c6f1f6ed
Matthew John Baumann <matthew.baumann@northwestern.edu>
2019-10-15 11:34:01 -0500

    WIP: change names and attribute accessing on saml_session_index methods. #204

--- app/controllers/saml_sessions_controller.rb
+++ app/controllers/saml_sessions_controller.rb
@@ -1,19 +1,19 @@
 # require "ruby-saml"
 
 class SamlSessionsController < Devise::SamlSessionsController
-  after_action :set_saml_session_index, only: :create
+  after_action :set_saml_session_index_on_session, only: :create
   skip_before_action :verify_authenticity_token, raise: false
   prepend_before_action :store_info_for_sp_initiated_logout, only: :destroy # FROM BRANCH FOR PR #149
-  prepend_before_action :get_saml_session_index, only: :destroy
+  prepend_before_action :set_saml_session_index_on_current_user, only: :destroy
 
   protected
 
-  def set_saml_session_index
-    session[Devise.saml_session_index_key] = current_user.send(Devise.saml_session_index_key) if user_signed_in?
+  def set_saml_session_index_on_session
+    session[Devise.saml_session_index_key] = current_user.read_attribute(Devise.saml_session_index_key) if user_signed_in?
   end
 
-  def get_saml_session_index
-    current_user.send(Devise.saml_session_index_key) = session[Devise.saml_session_index_key]
+  def set_saml_session_index_on_current_user
+    current_user.update_attribute(Devise.saml_session_index_key, session[Devise.saml_session_index_key])
   end
 
   # THESE FOLLOWING TWO METHODS WERE TAKEN FROM THE BRANCH OF DEVISE_SAML_AUTHENTICATABLE IN PR #149.
