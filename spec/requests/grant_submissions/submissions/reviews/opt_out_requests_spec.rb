10131704bc6204c682d212db3db928c4e482a072
jseraf <jseraf@users.noreply.github.com>
2019-12-05 14:33:17 -0600

    Request specs for submission review #301
    PR #312

---
+++ spec/requests/grant_submissions/submissions/reviews/opt_out_requests_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'grant_submission review opt_out requests', type: :request do
+  context '#delete' do
+    before(:each) do
+      ActionMailer::Base.deliveries.clear
+
+      @grant    = create(:open_grant_with_users_and_form_and_submission_and_reviewer)
+      @admin    = @grant.grant_permissions.where(role: 'admin').first.user
+      @assigner = @grant.grant_permissions.where(role: 'editor').first.user
+      @reviewer = @grant.reviewers.first
+      @review   = create(:review, submission: @grant.submissions.first,
+                                  assigner: @assigner,
+                                  reviewer: @reviewer)
+
+      login_as @reviewer
+    end
+
+    it 'mails the assigner' do
+      delete(opt_out_grant_submission_review_path(id: @review.id,
+                                                  grant_id: @grant.id,
+                                                  submission_id: @grant.submissions.first.id))
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+      email = (ActionMailer::Base.deliveries).first
+      expect(email.to).to eq([@assigner.email])
+      expect(email.subject).to eq('CD2H Competitions: Reviewer Opt Out Notification')
+    end
+
+    it 'mails grant admin when the assigner no longer has grant permissions' do
+      @grant.grant_permissions.find_by(user: @assigner).delete
+
+      delete(opt_out_grant_submission_review_path(id: @review.id,
+                                                  grant_id: @grant.id,
+                                                  submission_id: @grant.submissions.first.id))
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+      email = (ActionMailer::Base.deliveries).first
+      expect(email.to).to eq([@admin.email])
+      expect(email.subject).to eq('CD2H Competitions: Reviewer Opt Out Notification')
+    end
+  end
+end
