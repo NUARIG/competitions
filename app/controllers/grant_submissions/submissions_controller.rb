9b278c6329853ca1965610f625329062d131f81c
jseraf <j-serafin@northwestern.edu>
2020-03-24 11:16:58 -0500

    Fix default sort column name #417

--- app/controllers/grant_submissions/submissions_controller.rb
+++ app/controllers/grant_submissions/submissions_controller.rb
@@ -5,7 +5,7 @@ module GrantSubmissions
     def index
       @grant   = GrantDecorator.new(@grant)
       @q       = policy_scope(GrantSubmission::Submission, policy_scope_class: GrantSubmission::SubmissionPolicy::Scope).ransack(params[:q])
-      @q.sorts = 'created_at desc' if @q.sorts.empty?
+      @q.sorts = 'updated_at desc' if @q.sorts.empty?
       @pagy, @submissions = pagy(@q.result, i18n_key: 'activerecord.models.submission')
     end
 
