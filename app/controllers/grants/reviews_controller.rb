68b2357b41a52e8ab2d20b26252725726aed4797
jseraf <j-serafin@northwestern.edu>
2020-04-21 15:56:36 -0500

    Add xlsx and spreadsheet view #459

--- app/controllers/grants/reviews_controller.rb
+++ app/controllers/grants/reviews_controller.rb
@@ -6,9 +6,18 @@ module Grants
       @grant = Grant.kept.friendly.find(params[:grant_id])
       authorize @grant, :grant_viewer_access?
 
-      @q              = Review.by_grant(@grant).ransack(params[:q])
-      @q.sorts        = 'applicant_last_name asc' if @q.sorts.empty?
-      @pagy, @reviews = pagy(@q.result, i18n_key: 'activerecord.models.review')
+      if request.format.html?
+        @q              = Review.by_grant(@grant).ransack(params[:q])
+        @q.sorts        = 'applicant_last_name asc' if @q.sorts.empty?
+        @pagy, @reviews = pagy(@q.result, i18n_key: 'activerecord.models.review')
+      elsif request.format.xlsx?
+        @reviews        = Review.with_grant_and_applicant.with_criteria_reviews.by_grant(@grant)
+      end
+
+      respond_to do |format|
+        format.html
+        format.xlsx
+      end
     end
   end
 end
