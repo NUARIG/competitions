c5f77923dda57db5e5e29824c0823b1492ef753c
Matthew John Baumann <matthew.baumann@northwestern.edu>
2020-05-27 15:46:43 -0500

    remove change in quotation marks. #438

--- db/migrate/20190725192124_change_user_organization_role_to_system_admin.rb
+++ db/migrate/20190725192124_change_user_organization_role_to_system_admin.rb
@@ -1,12 +1,12 @@
 class ChangeUserOrganizationRoleToSystemAdmin < ActiveRecord::Migration[5.2]
   def up
     rename_column :users, :organization_role, :system_admin
-    change_column :users, :system_admin, "boolean USING (CASE system_admin WHEN \'admin\' THEN \'t\'::boolean ELSE \'f\'::boolean END)", null: false, default: false
+    change_column :users, :system_admin, 'boolean USING (CASE system_admin WHEN \'admin\' THEN \'t\'::boolean ELSE \'f\'::boolean END)', null: false, default: false
   end
 
   def down
     rename_column :users, :system_admin, :organization_role
-    change_column :users, :organization_role, "varchar(255) USING (CASE when organization_role THEN \'admin\' else \'none\' end)", null: false, default: 'none'
+    change_column :users, :organization_role, 'varchar(255) USING (CASE when organization_role THEN \'admin\' else \'none\' end)', null: false, default: 'none'
   end
 end
 
