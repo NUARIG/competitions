570883b0531505af3e52e9a7098c925d92b403d3
mattbaumann1 <mattbaumann1@users.noreply.github.com>
2020-06-11 10:10:46 -0500

    Task/remove draft grants from my reviews 355 (#524)
    * added conditionals for whether a grant is published to review index. #355
    * add specs for draft grant review UI. #355

--- spec/system/profiles/reviews_spec.rb
+++ spec/system/profiles/reviews_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe 'Profile Reviews', type: :system, js: true do
   let(:grant2_review)      { create(:scored_review_with_scored_mandatory_criteria_review, submission: grant2.submissions.first,
                                                                                           assigner: grant2.grant_permissions.role_admin.first.user,
                                                                                           reviewer: grant2_reviewer.reviewer)}
+  let(:draft_grant)             { create(:draft_open_grant_with_users_and_form_and_submission_and_reviewer, name: "Second #{Faker::Lorem.sentence(word_count: 3)}") }
+  let(:draft_grant_reviewer)    { create(:grant_reviewer, grant: draft_grant,
+                                                     reviewer: reviewer) }
+  let(:draft_grant_review)      { create(:scored_review_with_scored_mandatory_criteria_review, submission: draft_grant.submissions.first,
+                                                                                          assigner: draft_grant.grant_permissions.role_admin.first.user,
+                                                                                          reviewer: draft_grant_reviewer.reviewer)}
   let(:user)               { create(:user) }
 
   describe 'header text' do
@@ -40,6 +46,7 @@ RSpec.describe 'Profile Reviews', type: :system, js: true do
   before(:each) do
     grant1_review
     grant2_review
+    draft_grant_review
 
     login_as reviewer
     visit profile_reviews_path
@@ -49,8 +56,10 @@ RSpec.describe 'Profile Reviews', type: :system, js: true do
     scenario 'it includes links to assigned reviews' do
       expect(page).to have_text(grant1.name)
       expect(page).to have_text(grant2.name)
+      expect(page).to have_text(draft_grant.name)
       expect(page).to have_link('Incomplete', href: grant_submission_review_path(grant1, grant1.submissions.first, grant1_review))
       expect(page).to have_link('Completed', href: grant_submission_review_path(grant2, grant2.submissions.first, grant2_review))
+      expect(page).not_to have_link('Completed', href: grant_submission_review_path(draft_grant, draft_grant.submissions.first, draft_grant_review))
     end
 
     scenario 'it can be filtered on grant name' do
