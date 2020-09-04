fe56e7c34abec8eb89dc8ffa18474cb3a3695319
Matthew John Baumann <matthew.baumann@northwestern.edu>
2020-09-04 10:50:58 -0500

    make restricted_domains configurable. #593

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -6,7 +6,7 @@ class RegisteredUser < User
   after_initialize :set_uid, if: :new_record?
 
   SAML_DOMAINS = COMPETITIONS_CONFIG[:saml_domains] || []
-  RESTRICTED_EMAIL_DOMAINS   = ['.xyz', '.top', '.website', '.space', '.online']
+  RESTRICTED_EMAIL_DOMAINS   = COMPETITIONS_CONFIG[:restricted_domains] || []
 
   validate  :cannot_register_with_saml_email
   validate  :cannot_register_with_spam_domain
