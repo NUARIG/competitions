4d94e09177071412d7de7997ab6e1e74d967cea7
jseraf <j-serafin@northwestern.edu>
2020-09-10 09:34:27 -0500

    Remove obsolete sort method - found during #603

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -80,16 +80,6 @@ module ApplicationHelper
     @user # && !Pundit.policy(current_user, @user).update?
   end
 
-  def sortable_header(column, title = nil)
-    column = column.to_s
-    title ||= column.titleize
-    is_sorting_column = (column == sort_column)
-    direction = is_sorting_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    url = url_for params.to_unsafe_hash.merge({:sort_column => column, :sort_direction => direction})
-    content_tag(:th, title, class: is_sorting_column ? "sorting_#{sort_direction}" : 'sorting',
-                onClick: "document.location.href='#{url}';".html_safe)
-  end
-
   def form_table_row(f, attr, &field)
     render(partial: "form_table_row", locals: {f: f, attr: attr, field: field})
   end
