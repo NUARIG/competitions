396edbb3a7209e0fa6ed2ac10ab5d4d8aa347bd7
jseraf <j-serafin@northwestern.edu>
2020-09-10 09:31:39 -0500

    Use application_name in title tag #603

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,7 +37,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def title_tag_content(page_title: '')
-    base_title = 'Competitions'
+    base_title = COMPETITIONS_CONFIG[:application_name]
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 
