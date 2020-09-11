0ef25b6066e8b514c79cdbcb607912eea5204253
jseraf <jseraf@users.noreply.github.com>
2020-09-11 10:42:12 -0500

    Nest devise related configs (#609)
    * Add devise key to config file, nest saml_authenticatable and registerable configs under it #607
    
    * Use new :devise configs in devise initializer and across app #607

--- lib/devise_saml_authenticatable/competitions_attribute_map_resolver.rb
+++ lib/devise_saml_authenticatable/competitions_attribute_map_resolver.rb
@@ -1,7 +1,7 @@
 module DeviseSamlAuthenticatable
   class CompetitionsAttributeMapResolver < DeviseSamlAuthenticatable::DefaultAttributeMapResolver
     def attribute_map
-      COMPETITIONS_CONFIG[:saml][:attribute_map]
+      COMPETITIONS_CONFIG[:devise][:saml_authenticatable][:attribute_map]
     end
   end
 end
