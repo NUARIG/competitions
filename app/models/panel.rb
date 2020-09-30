1b895169276094cb845306e2dfb6eb45b954a763
jseraf <j-serafin@northwestern.edu>
2020-09-30 10:44:40 -0500

    Initial model validations and specs #627

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -1,14 +1,32 @@
 class Panel < ApplicationRecord
+  has_paper_trail versions: { class_name: 'PaperTrail::PanelVersion' },
+                  meta:     { grant_id: :grant_id }
+
   belongs_to :grant
+
+  validates_presence_of :start_datetime,  if: -> { end_datetime? },
+                                          message: 'is required if end is provided.'
+  validates_presence_of :end_datetime,    if: -> { start_datetime? },
+                                          message: 'is required if start is provided.'
 
   validates_datetime :start_datetime, before: :end_datetime,
                                       if: -> { start_datetime? || end_datetime? }
 
   validates_uniqueness_of :grant
+  validates :meeting_link,  if: -> { meeting_link? },
+                            format: { with:     /\A(https:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$)\z/ix,
+                                      message: 'is not a valid secure URL.' }
 
   validate :start_is_after_submission_deadline, if: :start_datetime?
 
+  def in_panel_mode?
+    return false if !start_datetime? || !end_datetime?
+    DateTime.now.between?(start_datetime, end_datetime)
+  end
+
+  private
+
   def start_is_after_submission_deadline
-    errors.add(:start_datetime, message: 'must be after the submission close date.') if start_datetime < grant.submission_close_date.end_of_day
+    errors.add(:start_datetime, :before_submission_deadline) if start_datetime < grant.submission_close_date.end_of_day
   end
 end
--- spec/models/panel_spec.rb
+++ spec/models/panel_spec.rb
@@ -8,7 +8,11 @@ RSpec.describe Panel, type: :model do
   it { is_expected.to respond_to(:meeting_link) }
   it { is_expected.to respond_to(:meeting_location) }
 
-  let(:panel) { create(:panel) }
+  let(:panel)         { create(:panel)          }
+  let(:open_panel)    { create(:open_panel)     }
+  let(:before_panel)  { create(:before_panel)   }
+  let(:after_panel)   { create(:after_panel)    }
+  let(:no_panel)      { create(:no_panel_dates) }
 
   describe '#validations' do
     it 'validates a valid panel' do
@@ -39,12 +43,14 @@ RSpec.describe Panel, type: :model do
         panel.start_datetime = nil
         expect(panel).not_to be_valid
         expect(panel.errors).to include(:start_datetime)
+        expect(panel.errors.full_messages).to include('Start Date/Time is required if end is provided.')
       end
 
       it 'require end_datetime if start_dateime is set' do
         panel.end_datetime = nil
         expect(panel).not_to be_valid
-        expect(panel.errors).to include(:start_datetime)
+        expect(panel.errors).to include(:end_datetime)
+        expect(panel.errors.full_messages).to include('End Date/Time is required if start is provided.')
       end
 
       it 'does not require end_datetime and start_dateime to be set' do
@@ -52,7 +58,52 @@ RSpec.describe Panel, type: :model do
         panel.end_datetime = nil
         expect(panel).to be_valid
       end
+    end
 
+    context 'meeting_link' do
+      it 'requires a valid secure url' do
+        panel.meeting_link = 'invalid'
+        expect(panel).not_to be_valid
+        expect(panel.errors.full_messages).to include 'Meeting Link is not a valid secure URL.'
+        panel.meeting_link = 'ftp://abc.com/'
+        expect(panel).not_to be_valid
+        expect(panel.errors.full_messages).to include 'Meeting Link is not a valid secure URL.'
+        panel.meeting_link = 'http://abc.com/'
+        expect(panel).not_to be_valid
+        expect(panel.errors.full_messages).to include 'Meeting Link is not a valid secure URL.'
+        panel.meeting_link = 'https://abc.com/'
+        expect(panel).to be_valid
+        panel.meeting_link = 'https://college.zoom.us/z/123456789'
+        expect(panel).to be_valid
+      end
+    end
+  end
+
+  describe '#methods' do
+    context '#panel_mode?' do
+      context 'open panel' do
+        it 'is true' do
+          expect(open_panel.in_panel_mode?).to be true
+        end
+      end
+
+      context 'before start_datetime' do
+        it 'is false' do
+          expect(before_panel.in_panel_mode?).to be false
+        end
+      end
+
+      context 'after end_datetime' do
+        it 'is false' do
+          expect(after_panel.in_panel_mode?).to be false
+        end
+      end
+
+      context 'no panel dates' do
+        it 'is false' do
+          expect(no_panel.in_panel_mode?).to be false
+        end
+      end
     end
   end
 end
