d3f3ec0166b8035874d7cad41f9b60e5ffd6c41b
Matthew John Baumann <matthew.baumann@northwestern.edu>
2020-10-07 13:16:39 -0500

    upgraded to 6.0.3.4. #279

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'pg'
-gem 'rails', '~> 6.0.3.3'
+gem 'rails', '~> 6.0.3.4'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,10 @@ Rails.application.configure do
   config.time_zone = 'Central Time (US & Canada)'
   config.active_record.default_timezone = :local
 
+  # Don't care if the mailer can't send.
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.perform_caching = false
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
