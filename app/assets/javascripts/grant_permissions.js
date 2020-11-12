9bbe6f4d70f728cb1fd46d1b6446ae8efe5e8f23
Matthew John Baumann <matthew.baumann@northwestern.edu>
2020-11-12 17:12:09 -0600

    add select2 autocomplete to grant_permissions. #372

--- app/assets/javascripts/grant_permissions.js
+++ app/assets/javascripts/grant_permissions.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $('select#grant_permission_user_id').select2({
+    placeholder: "Choose a person",
+    allowClear: true
+  });
+});
