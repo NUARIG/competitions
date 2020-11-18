f627a48374867dfe034f389b9e278ce59df92476
jseraf <j-serafin@northwestern.edu>
2020-11-18 15:29:05 -0600

    Update or add specs for grant_reviewer/invitations #699

---
+++ spec/system/grant_reviewers/grant_reviewer_invitations_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe GrantReviewer::Invitation, type: :system do
+  context 'paper trail' do
+    pending 'it tracks whodunnit' do
+      fail 'Ensure inviter/whodunnit is tracked'
+    end
+  end
+end
