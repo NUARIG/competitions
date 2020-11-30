5b1769daf9b4c52e70a2faf374104297d7aae1c4
jseraf <j-serafin@northwestern.edu>
2020-11-30 16:16:58 -0600

    Add reviewer invitation related specs to user models #710

--- spec/models/registered_user_spec.rb
+++ spec/models/registered_user_spec.rb
@@ -21,9 +21,11 @@ RSpec.describe RegisteredUser, type: :model do
   it { is_expected.to respond_to(:last_name) }
   it { is_expected.to respond_to(:grant_creator) }
   it { is_expected.to respond_to(:era_commons) }
+  it { is_expected.to respond_to(:pending_reviewer_invitations) }
 
   let(:user)       { FactoryBot.build(:registered_user) }
   let(:other_user) { create(:registered_user, era_commons: Faker::Lorem.characters(number: 10)) }
+  let(:reviewer_invitation) { create(:grant_reviewer_invitation, email: user.email) }
 
   describe '#initiations' do
     it 'sets default of system_admin boolean' do
@@ -89,10 +91,47 @@ RSpec.describe RegisteredUser, type: :model do
       expect(user).not_to be_valid
       expect(user.errors).to include :era_commons
     end
+  end
 
-    # pending
-    # expect an auth gem to handle this
-    it 'validates an email'
-    it 'validate unique email'
+  context 'reviewer invitations' do
+    describe '#methods' do
+      context 'set_pending_reviewer_invitations' do
+        it 'has no pending_reviewer_invitations' do
+          user.set_pending_reviewer_invitations
+          expect(user.pending_reviewer_invitations.empty?).to be true
+        end
+
+        context 'with pending invitation' do
+          it 'sets pending_reviewer_invitations' do
+            reviewer_invitation.save
+            user.set_pending_reviewer_invitations
+            expect(user.pending_reviewer_invitations.empty?).to be false
+          end
+        end
+      end
+
+      context 'process_pending_reviewer_invitations' do
+        before(:each) do
+          reviewer_invitation.save
+        end
+
+        it 'confirms the reviewer_invitation' do
+          expect(reviewer_invitation.confirmed_at.nil?).to be true
+          user.save
+          expect(reviewer_invitation.reload.confirmed_at.nil?).to be false
+        end
+
+        it 'creates the grant_reviewer' do
+          expect do
+            user.save
+          end.to change{reviewer_invitation.grant.reviewers.count}.by(1)
+        end
+
+        it 'confirms the user' do
+          user.save
+          expect(user.confirmed_at).not_to be nil
+        end
+      end
+    end
   end
 end
