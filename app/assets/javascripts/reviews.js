c42263354fcc55f654cea3ad1355b190b65159ca
Matthew Baumann <matthew.baumann@northwestern.edu>
2021-01-29 11:47:13 -0600

    add criterion score clear button. #726

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,7 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $('button.criterion-clear-button').click(function () {
+    $clearButton = $(this)
+    $criterionScoringContainer = $clearButton.closest('.criterion-scoring')
+    $criterionScoringContainer.find('input:checked').prop('checked', false);
+  });
+});
