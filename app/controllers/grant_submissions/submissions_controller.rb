e4f5a570b5d91af467e57d7c8eacda50ba8893f6
jseraf <jseraf@users.noreply.github.com>
2021-03-18 15:16:44 -0500

    Hotfix/submission export limited by pagy settings (#800)
    * Separate .xlsx queries to bypass pagy item limits #795
    
    * Move questions query into xlsx format #795
    
    * Remove unneeded responses submission query for html format #795

--- app/controllers/grant_submissions/submissions_controller.rb
+++ app/controllers/grant_submissions/submissions_controller.rb
@@ -4,12 +4,16 @@ module GrantSubmissions
 
     def index
       @grant      = Grant.kept.friendly.with_administrators.find(params[:grant_id])
-      @questions  = @grant.questions
 
       if Pundit.policy(current_user, @grant).show?
-        @q       = policy_scope(GrantSubmission::Submission.with_responses, policy_scope_class: GrantSubmission::SubmissionPolicy::Scope).ransack(params[:q])
-        @q.sorts = 'user_updated_at desc' if @q.sorts.empty?
-        @pagy, @submissions = pagy(@q.result, i18n_key: 'activerecord.models.submission')
+        if params[:format] == 'xlsx'
+          @questions  = @grant.questions
+          @submissions = policy_scope(GrantSubmission::Submission.with_responses, policy_scope_class: GrantSubmission::SubmissionPolicy::Scope)
+        else
+          @q       = policy_scope(GrantSubmission::Submission, policy_scope_class: GrantSubmission::SubmissionPolicy::Scope).ransack(params[:q])
+          @q.sorts = 'user_updated_at desc' if @q.sorts.empty?
+          @pagy, @submissions = pagy(@q.result, i18n_key: 'activerecord.models.submission')
+        end
       else
         skip_policy_scope
         flash[:alert] = I18n.t('pundit.default')
