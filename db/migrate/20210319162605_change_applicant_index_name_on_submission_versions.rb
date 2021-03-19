aecbdc939c3320fcfb742bde5ba8ae44458688a6
Matthew Baumann <matthew.baumann@northwestern.edu>
2021-03-19 12:37:14 -0500

    change name and split index name change to separate migration. #785

--- db/migrate/20210319162605_change_applicant_index_name_on_submission_versions.rb
+++ db/migrate/20210319162605_change_applicant_index_name_on_submission_versions.rb
@@ -1,6 +1,5 @@
-class ChangeApplicantColumnNameOnSubmissions < ActiveRecord::Migration[6.0]
+class ChangeApplicantIndexNameOnSubmissionVersions < ActiveRecord::Migration[6.0]
   def change
-    rename_column :grant_submission_submission_versions, :applicant_id, :submitter_id
     rename_index :grant_submission_submission_versions, 'index_gs_submission_v_on_grant_id_applicant_it', 'index_gs_submission_v_on_grant_id_submitter_it'
   end
 end
