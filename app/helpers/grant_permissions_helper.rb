a7e9226f4a89723495cbf7fa4913c21acc245fd7
jseraf <j-serafin@northwestern.edu>
2021-03-25 20:38:19 -0500

    Add admin_submission_notification_emails helper method #801

--- app/helpers/grant_permissions_helper.rb
+++ app/helpers/grant_permissions_helper.rb
@@ -4,4 +4,11 @@ module GrantPermissionsHelper
   def grant_permission_role_select_options
     GrantPermission::ROLES.values.map{ |permission| [permission.capitalize, permission]}
   end
+
+  def admin_submission_notification_emails(grant:)
+    grant.grant_permissions
+         .with_user
+         .where(submission_notification: true)
+         .map{ |permission| permission.user.email }
+  end
 end
