e230d7e46c2eb0c7cbcf40685761c25d575aa6df
jseraf <j-serafin@northwestern.edu>
2021-03-25 20:44:07 -0500

    Update, add specs around changes in #801

--- spec/helpers/grant_permissions_helper_spec.rb
+++ spec/helpers/grant_permissions_helper_spec.rb
@@ -1,17 +1,29 @@
 # frozen_string_literal: true
-
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the GrantPermissionsHelper. For example:
-#
-# describe GrantPermissionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe GrantPermissionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:grant) { create(:published_open_grant_with_users) }
+
+  context 'admin_submission_notification_emails' do
+    context 'no submission_notifiction admins' do
+      it 'returns [] when no admins' do
+        expect(admin_submission_notification_emails(grant: grant)).to be_empty
+      end
+    end
+
+    context 'one submission_notifiction admin' do
+      it 'returns array with one email' do
+        grant.grant_permissions.role_admin.first.update(submission_notification: true)
+        expect(admin_submission_notification_emails(grant: grant)).to eql [grant.admins.first.email]
+      end
+    end
+
+    context 'multiple submission_notifiction admins' do
+      it 'returns array with all emails' do
+        grant.grant_permissions.role_admin.first.update(submission_notification: true)
+        grant.grant_permissions.role_viewer.first.update(submission_notification: true)
+        expect(admin_submission_notification_emails(grant: grant)).to eql [grant.admins.first.email, grant.viewers.first.email]
+      end
+    end
+  end
 end
