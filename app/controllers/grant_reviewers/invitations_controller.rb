e7155a8867cee6ac2e6813b14d19ebadc8a6b790
jseraf <jseraf@users.noreply.github.com>
2021-03-25 20:48:22 -0500

    Add simple reviewer invitation index view #770 (#802)

--- app/controllers/grant_reviewers/invitations_controller.rb
+++ app/controllers/grant_reviewers/invitations_controller.rb
@@ -1,11 +1,11 @@
 module GrantReviewers
   class InvitationsController < ApplicationController
     before_action :set_grant
+    skip_after_action :verify_policy_scoped, only: %i[index]
 
     def index
       authorize @grant, :edit?
-      @invitations = GrantReviewer::Invitation.by_grant(@grant)
-      skip_after_action :verify_policy_scoped
+      @invitations = GrantReviewer::Invitation.with_inviter.by_grant(@grant)
     end
 
     def update;end
@@ -15,7 +15,7 @@ module GrantReviewers
       invitation = GrantReviewer::Invitation.new(grant: @grant, inviter: current_user, email: params[:email])
       if invitation.save
         GrantReviewerInvitationMailer.invite(invitation: invitation, grant: @grant, inviter: current_user).deliver_now
-        flash[:alert] = 'Invitation created.'
+        flash[:alert] = "Invitation to #{invitation.email} has been sent. #{helpers.link_to 'View all invitations', grant_invitations_path(@grant)}"
       else
         flash[:warning] = invitation.errors.full_messages
       end
