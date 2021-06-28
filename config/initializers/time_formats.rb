4e69aba30947a72d09ef3277f224604aca4244fe
jseraf <j-serafin@northwestern.edu>
2021-06-28 09:39:40 -0500

    Add machine format for date and time

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -5,5 +5,11 @@
 Date::DATE_FORMATS[:default] = '%m/%d/%Y'
 # short_format: -M/-D/YY
 Date::DATE_FORMATS[:short_format] = '%-m/%-d/%y'
-# Time
+# machine: YYYY-MM-DD
+Date::DATE_FORMATS[:machine] = '%Y-%m-%d'
+
+# Times
+# Default: MM/DD/YYYY #:##AM
 Time::DATE_FORMATS[:default] = '%m/%d/%Y %l:%M%P'
+# Machine: YYYY-MM-DD
+Time::DATE_FORMATS[:machine] = '%Y-%m-%d %T'
