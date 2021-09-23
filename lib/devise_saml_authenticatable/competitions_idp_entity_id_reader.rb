400b913f0468a80e5b59bb72167b8cab87efb650
jseraf <j-serafin@northwestern.edu>
2021-09-23 12:39:39 -0500

    WIP: seeing what sticks

--- lib/devise_saml_authenticatable/competitions_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/competitions_idp_entity_id_reader.rb
@@ -13,7 +13,7 @@
 #             allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
 #           )
 #         doc = REXML::Document.new(response.response)
-
+#
 #         if REXML::XPath.match(doc,"/p:LogoutResponse",{ "p" => OneLogin::RubySaml::SamlMessage::PROTOCOL}).any?
 #           REXML::XPath.match(doc,"/p:LogoutResponse/a:Issuer",{ "p" => OneLogin::RubySaml::SamlMessage::PROTOCOL, "a" => OneLogin::RubySaml::SamlMessage::ASSERTION }).first.text
 #         else
