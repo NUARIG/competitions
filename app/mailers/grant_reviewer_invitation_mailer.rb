4ca1772fefa10611eb8908913e338008c7a1d3a9
jseraf <j-serafin@northwestern.edu>
2021-09-30 12:17:59 -0500

    Move get_login_url_by_email_address to helper method

--- app/mailers/grant_reviewer_invitation_mailer.rb
+++ app/mailers/grant_reviewer_invitation_mailer.rb
@@ -2,7 +2,6 @@ class GrantReviewerInvitationMailer < ApplicationMailer
   def invite(invitation:, grant:, inviter:)
     @email      = invitation.email
     @grant      = grant
-    @login_url  = get_login_url_by_email_address(@email)
     @inviter    = inviter
 
     mail(to: @email, subject: I18n.t('mailers.grant_reviewer_invitations.invite.subject', grant: @grant.name))
@@ -11,15 +10,8 @@ class GrantReviewerInvitationMailer < ApplicationMailer
   def reminder(invitation:, grant:)
     @email      = invitation.email
     @grant      = grant
-    @login_url  = get_login_url_by_email_address(@email)
     @inviter    = invitation.inviter
 
     mail(to: @email, subject: I18n.t('mailers.grant_reviewer_invitations.reminder.subject', grant: @grant.name))
-  end
-
-  private
-
-  def get_login_url_by_email_address(email)
-    User.is_saml_email_address?(email: email) ? login_index_url : new_registered_user_registration_url
   end
 end
