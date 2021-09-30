5583e7c87615f25edfbcb6963e7a0c1a451efa9b
jseraf <j-serafin@northwestern.edu>
2021-09-30 12:18:22 -0500

    Add specs for get_login_url_by_email_address helper

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -2,16 +2,17 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:saml_user_email)       { Faker::Internet.email(domain: COMPETITIONS_CONFIG[:devise][:registerable][:saml_domains].last) }
+  let(:registered_user_email) { Faker::Internet.email }
+
+  context '#get_login_url_by_email_address' do
+    it 'returns SAML login when given a configured saml user domain' do
+      expect(get_login_url_by_email_address(saml_user_email)).to eql login_index_url
+    end
+
+    it 'returns Devise login url when given a configured non-saml domain' do
+      expect(get_login_url_by_email_address(registered_user_email)).to eql new_registered_user_registration_url
+    end
+  end
 end
