5847c4a8618a1863a54c4d8d8ef0ef0aa64e7503
jseraf <jseraf@users.noreply.github.com>
2021-10-01 09:15:10 -0500

    Task/nudge reviewer invitations (#865)
    * Add Send Reminders link/email to reviewer invitations
    
    * Move get_login_url_by_email_address to helper method
    
    * Add specs

--- app/mailers/grant_reviewer_invitation_mailer.rb
+++ app/mailers/grant_reviewer_invitation_mailer.rb
@@ -2,9 +2,16 @@ class GrantReviewerInvitationMailer < ApplicationMailer
   def invite(invitation:, grant:, inviter:)
     @email      = invitation.email
     @grant      = grant
-    @login_url  = User.is_saml_email_address?(email: @email) ? login_index_url : new_registered_user_registration_url
     @inviter    = inviter
 
-    mail(to: @email, subject: "Reviewer Invitation - #{@grant.name}")
+    mail(to: @email, subject: I18n.t('mailers.grant_reviewer_invitations.invite.subject', grant: @grant.name))
+  end
+
+  def reminder(invitation:, grant:)
+    @email      = invitation.email
+    @grant      = grant
+    @inviter    = invitation.inviter
+
+    mail(to: @email, subject: I18n.t('mailers.grant_reviewer_invitations.reminder.subject', grant: @grant.name))
   end
 end
