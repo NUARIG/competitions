4dc1f298e63ed84b65045de9b816df11f523fd34
Matthew Baumann <matthew.baumann@northwestern.edu>
2021-10-05 10:00:43 -0500

    add first login system spec. #696

--- spec/system/login/login_spec.rb
+++ spec/system/login/login_spec.rb
@@ -5,6 +5,13 @@ include UsersHelper
 
 RSpec.describe 'Login', type: :system do
 
+  let(:test_saml_user)    { create(:saml_user,
+                                    first_name: "#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:first_name]}",
+                                    last_name: "#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:last_name]}",
+                                    email: "#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:email]}",
+                                    uid: "#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:uid]}",
+                                    current_sign_in_at: Time.now) }
+
   describe 'Home page', js: true do
     scenario 'home page login link navigates to login page' do
       visit root_path
@@ -14,35 +21,81 @@ RSpec.describe 'Login', type: :system do
     end
   end
 
-  describe 'Login page', js: true do
-    before(:each) do
-      visit login_index_path
-    end
+  context 'saml users' do
+    describe 'Login page', js: true do
+      before(:each) do
+        visit login_index_path
+      end
 
-    scenario 'login page has SAML ID login button' do
-      expect(page).to have_selector(:link_or_button, "With #{COMPETITIONS_CONFIG[:devise][:saml_authenticatable][:idp_entity_name]}")
-    end
+      scenario 'login page has SAML ID login button' do
+        expect(page).to have_selector(:link_or_button, "With #{COMPETITIONS_CONFIG[:devise][:saml_authenticatable][:idp_entity_name]}")
+      end
 
-    scenario 'login page has Registered Account login form' do
-      expect(page).to have_selector('#registered_user_uid')
-      expect(page).to have_selector('#registered_user_password')
+      # The following specs runs against values set in the
+      # config/competitions_config.yml find(:css, "#username")
+      # and find(:css, "#password") are based on the
+      # dev/test set up according to our wiki
+      # (https://github.com/NUARIG/competitions/wiki/Installation-and-configuration-of-test-saml-idp).
+      # The css IDs might vary depending on whether you are using
+      # a similar docker SAML setup.
+      scenario 'login using SAML IDP authentication' do
+        expect(page).to have_selector(:link_or_button, "With #{COMPETITIONS_CONFIG[:devise][:saml_authenticatable][:idp_entity_name]}")
+        find("#saml_login_link_button").click
+
+        find(:css, "#username").set("#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:uid]}")
+        find(:css, "#password").set("#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:password]}")
+        click_button 'Login'
+
+        expect(page).to have_content 'Signed in successfully.'
+        expect(page).to have_content "#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:first_name]} #{COMPETITIONS_CONFIG[:devise][:test_saml_user][:last_name]}"
+      end
+
+      # scenario 'logout using SAML IDP authentication' do
+
+      # end
+
+      # scenario 'creates a new user if found in the SAML IDP' do
+      #   find("#saml_login_link_button").click
+      #   find(:css, "#username").set("#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:uid]}")
+      #   find(:css, "#password").set("#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:password]}")
+      #   click_button 'Login'
+
+      #   expect(page).to have_content 'Signed in successfully.'
+      #   expect(page).to have_content "#{COMPETITIONS_CONFIG[:devise][:test_saml_user][:first_name]} #{COMPETITIONS_CONFIG[:devise][:test_saml_user][:last_name]}"
+
+
+      # end
     end
   end
 
-  describe 'deep linking on log in', js: true do
-    let(:grant)             { create(:open_grant_with_users_and_form_and_submission_and_reviewer) }
-    let(:registered_editor)   { create(:registered_user) }
-    let(:editor_permission)   { create(:grant_permission, grant: grant, user: registered_editor) }
+  context 'registered users' do
+    describe 'Login page', js: true do
+      before(:each) do
+        visit login_index_path
+      end
 
-    context 'log in to grant reviews index' do
-      scenario 'it redirects to the deep link on login' do
-        editor_permission
-        visit grant_reviews_path(grant)
-        find(:css, "#registered_user_uid").set(registered_editor.email)
-        find(:css, "#registered_user_password").set(registered_editor.password)
-        click_button 'Log in'
-        expect(page).to have_current_path grant_reviews_path(grant)
+      scenario 'login page has Registered Account login form' do
+        expect(page).to have_selector('#registered_user_uid')
+        expect(page).to have_selector('#registered_user_password')
+      end
+    end
+
+    describe 'deep linking on log in', js: true do
+      let(:grant)             { create(:open_grant_with_users_and_form_and_submission_and_reviewer) }
+      let(:registered_editor)   { create(:registered_user) }
+      let(:editor_permission)   { create(:grant_permission, grant: grant, user: registered_editor) }
+
+      context 'log in to grant reviews index' do
+        scenario 'it redirects to the deep link on login' do
+          editor_permission
+          visit grant_reviews_path(grant)
+          find(:css, "#registered_user_uid").set(registered_editor.email)
+          find(:css, "#registered_user_password").set(registered_editor.password)
+          click_button 'Log in'
+          expect(page).to have_current_path grant_reviews_path(grant)
+        end
       end
     end
   end
+
 end
