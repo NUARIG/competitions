38ef9e34cd484b5de48a6b45ba3cc4d99d0f11f7
Matthew Baumann <matthew.baumann@northwestern.edu>
2021-11-10 17:03:39 -0600

    foundation needed to load with turbo. #881

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 // import "./channels"
 import "@hotwired/turbo-rails"
+// import "@rails/actioncable"
 
 import "./src/vendor/jquery"
 import "jquery-ui"
@@ -12,6 +13,7 @@ import "trix"
 import "@rails/actiontext"
 import "foundation-sites"
 import "foundation-datepicker"
+import "motion-ui"
 import "select2"
 
 import "./controllers"
@@ -20,7 +22,7 @@ Rails.start()
 ActiveStorage.start()
 
 require('./src/legacy/app')
-require('./src/legacy/cable')
+// require('./src/legacy/cable')
 require('./src/legacy/dependency')
 require('./src/legacy/form_builder_standard_answer')
 require('./src/legacy/form_builder_survey')
@@ -28,3 +30,7 @@ require('./src/legacy/form_builder_upload_setup')
 require('./src/legacy/foundation-datepicker')
 require('./src/legacy/grant_permissions')
 require('./src/legacy/reviews')
+
+$(document).on('turbo:load', function() {
+    $(function(){ $(document).foundation(); });
+});
