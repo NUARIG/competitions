3699df7eadfa20482f3d892ef7e8446acc8027ac
jseraf <j-serafin@northwestern.edu>
2022-10-25 09:51:34 -0500

    Move role descriptions to select options

--- app/helpers/grant_permissions_helper.rb
+++ app/helpers/grant_permissions_helper.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 module GrantPermissionsHelper
+  ROLE_DESCRIPTIONS = { 'Admin':  'Delete or edit this grant and its submissions and reviews.',
+                        'Editor': 'Edit this grant and its submissions and reviews.',
+                        'Viewer': 'View this grant\'s submissions and reviews.' }
+
+
   def grant_permission_role_select_options
-    GrantPermission::ROLES.values.map{ |permission| [permission.capitalize, permission]}
+    GrantPermission::ROLES.values.map{ |role| ["#{role.capitalize} - #{ROLE_DESCRIPTIONS[role.capitalize.to_sym]}", role] }
   end
 
   def admin_submission_notification_emails(grant:)
