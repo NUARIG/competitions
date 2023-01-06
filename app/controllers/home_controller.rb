14689712639ac597fe9cb4b63b0f8db9280a4daf
jseraf <j-serafin@northwestern.edu>
2023-01-06 09:31:47 -0600

    Remove all things decorator from index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @grants = Grants::PublicDecorator.decorate_collection(Grant.kept.public_grants)
+    @grants = Grant.kept.public_grants
     skip_policy_scope
   end
 end
