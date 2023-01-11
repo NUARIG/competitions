df720e26b89925237cb71ae595c9b9a742c7e86d
jseraf <j-serafin@northwestern.edu>
2023-01-11 11:52:04 -0600

    Minimize spec set-up

--- spec/system/home/home_spec.rb
+++ spec/system/home/home_spec.rb
@@ -1,95 +1,114 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 include UsersHelper
+include ActionView::RecordIdentifier
 
 RSpec.describe 'Home', type: :system do
-  LONG_NAME = Faker::Lorem.paragraph_by_chars(number: 156)
-
-  let!(:open_grant)       { create(:grant_with_users) }
-  let!(:closed_grant)     { create(:published_closed_grant) }
-  let!(:completed_grant)  { create(:completed_grant) }
-  let!(:draft_grant)      { create(:draft_open_grant) }
-  let!(:discarded_grant)  { create(:grant_with_users, discarded_at: 1.hour.ago) }
-  let!(:user)             { create(:saml_user) }
-  let!(:admin_user)       { open_grant.administrators.first }
-  let(:long_name_grant)   { create(:grant_with_users, name: LONG_NAME)}
+  let(:open_grant)        { create(:grant_with_users) }
+  let(:closed_grant)      { create(:published_closed_grant) }
+  let(:completed_grant)   { create(:completed_grant) }
+  let(:draft_grant)       { create(:draft_open_grant) }
+  let(:discarded_grant)   { create(:grant_with_users, discarded_at: 1.hour.ago) }
+  let(:user)              { create(:saml_user) }
+  let(:admin_user)        { open_grant.administrators.first }
+  let(:long_name_grant)   { create(:grant_with_users, name: Faker::Lorem.paragraph_by_chars(number: 156))}
   let(:second_open_grant) { create(:grant_with_users) }
 
 
   describe 'Index', js: true do
-    before(:each) do
-      # @open_grant         = create(:grant_with_users)
-      # @closed_grant       = create(:published_closed_grant)
-      # @completed_grant    = create(:completed_grant)
-      # @draft_grant        = create(:draft_open_grant)
-      # @discarded_grant    = create(:grant_with_users, discarded_at: 1.hour.ago)
-      # @user               = create(:saml_user)
-      # @admin_user         = @open_grant.administrators.first
-      visit root_path
-    end
-
     describe 'header links' do
       context 'anonymous user' do
+        before(:each) do
+          visit root_path
+        end
+
         scenario 'displays login link' do
-          expect(page).to have_button 'Log In'
+          within('header') do
+            expect(page).to have_button 'Log In'
+          end
         end
 
         scenario 'displays help log in links' do
-          expect(page).to have_link 'Help'
-          page.find('#help').hover
-          expect(page).to have_link 'Release Notes'
+          within('header') do
+            expect(page).to have_link 'Help'
+            page.find('#help').hover
+            expect(page).to have_link 'Release Notes'
+          end
         end
       end
 
       context 'logged in user' do
-        scenario 'displays user name and profile link' do
+        before(:each) do
           login_as(user, scope: :saml_user)
           visit root_path
-          expect(page).to have_link full_name(user), href: '#'
-          page.find('#logged-in').hover
-          expect(page).to have_link 'Edit Your Profile'
         end
-      end
 
-      scenario 'does not display help log in link' do
-        login_as(user, scope: :saml_user)
-        visit root_path
-        expect(page).to have_link 'Help'
-        page.find('#help').hover
-        expect(page).not_to have_link 'Logging In'
+        scenario 'displays user name and profile link' do
+          within('header') do
+            expect(page).to have_link full_name(user), href: '#'
+            page.find('#logged-in').hover
+            expect(page).to have_link 'Edit Your Profile'
+          end
+        end
+
+        scenario 'does not display help log in link' do
+          within('header') do
+            expect(page).to have_link 'Help'
+            page.find('#help').hover
+            expect(page).not_to have_link 'Logging In'
+          end
+        end
       end
     end
 
     describe 'grant display logic' do
+      before(:each) do
+        open_grant.touch
+        closed_grant.touch
+        completed_grant.touch
+        draft_grant.touch
+        discarded_grant.touch
+
+        visit root_path
+      end
+
       scenario 'does not require a login' do
         expect(page).not_to have_content 'You are not authorized to perform this action.'
       end
 
       scenario 'displays a published grant' do
-        expect(page).to have_content open_grant.name
+        within('#public-grants') do
+          expect(page).to have_content open_grant.name
+        end
       end
 
       scenario 'does not display a closed grant' do
-        expect(page).not_to have_content closed_grant.name
+        within('#public-grants') do
+          expect(page).not_to have_content closed_grant.name
+        end
       end
 
       scenario 'does not display a completed grant' do
-        expect(page).not_to have_content completed_grant.name
+        within('#public-grants') do
+          expect(page).not_to have_content completed_grant.name
+        end
       end
 
       scenario 'does not display a draft grant' do
-        expect(page).not_to have_content draft_grant.name
+        within('#public-grants') do
+          expect(page).not_to have_content draft_grant.name
+        end
       end
 
       scenario 'does not display a soft_deleted grant' do
-        expect(page).not_to have_content discarded_grant.name
+        within('#public-grants') do
+          expect(page).not_to have_content discarded_grant.name
+        end
       end
     end
 
     describe 'edit grant link logic' do
       before(:each) do
-        second_open_grant.save
+        second_open_grant.touch
         login_as(admin_user, scope: :saml_user)
         visit root_path
       end
@@ -102,13 +121,18 @@ RSpec.describe 'Home', type: :system do
 
     describe 'grant name truncation logic' do
       before(:each) do
-        long_name_grant.save
+        long_name_grant.touch
         visit root_path
       end
 
       scenario 'truncates grant names longer than 150 characters' do
-        expect(page).not_to have_content LONG_NAME
-        expect(page).to have_content (LONG_NAME.first(147) + '...')
+        within("##{dom_id(long_name_grant)}") do
+          expect(page).not_to have_content long_name_grant.name
+          expect(page).to have_content (long_name_grant.name.first(147) + '...')
+        end
+      end
+    end
+
       end
     end
   end
