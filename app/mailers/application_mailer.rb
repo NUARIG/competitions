2fe0217a674179df964886ad06612171499ac501
jseraf <jseraf@users.noreply.github.com>
2023-01-17 13:54:41 -0600

    Put all mailers in their own app/views directory (#997)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  prepend_view_path "app/views/mailers"
+
   default from: COMPETITIONS_CONFIG[:mailer][:email]
+
   layout 'mailer'
+
   helper :users
 end
