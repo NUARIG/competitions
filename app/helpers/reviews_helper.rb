5cdd16418b92121db9d0fa01f574a36e7acd1fff
jseraf <jseraf@users.noreply.github.com>
2023-09-29 11:49:38 -0500

    Task/fix review criteria show alignment (#1018)
    * Add shared partial to display review scores/comments on submission admin/panel pages
    
    * Add No Score abbreviation tag helper
    
    * Use new review score/comment partial in admin submission review index pages
    
    * Tweak line-height css in table headers

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,4 +2,8 @@ module ReviewsHelper
   def find_review_by_submission_and_reviewer(submission, reviewer)
     Review.find_by(submission: submission, reviewer: reviewer)
   end
+
+  def no_score_abbreviation
+    content_tag(:abbr, 'NS', title: 'No Score')
+  end
 end
