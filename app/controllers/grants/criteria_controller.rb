b72aefbb9f413c2bb2df0172a39aaf1ed3fe92c3
jseraf <jseraf@users.noreply.github.com>
2023-09-29 11:46:28 -0500

    task/disable review form edits after completed reviews (#1015)
    * Disallow criteria update when grant has completed reviews
    
    * Clarify disabled/editable form status variable name
    
    * Misc formatting, typo correction, and spec improvement

--- app/controllers/grants/criteria_controller.rb
+++ app/controllers/grants/criteria_controller.rb
@@ -6,12 +6,23 @@ module Grants
     skip_after_action :verify_policy_scoped, only: %i[index]
 
     def index
+      @grant = Grant.includes(:reviews, :criteria).kept.friendly.find(params[:id])
+
       authorize @grant, :grant_viewer_access?
+
       @criteria = @grant.criteria.order(:created_at)
+      # disable_fields == true when either:
+      #  current_user does not have grant_editor_access permissions
+      #  or
+      #  there are completed reviews
+      @editable = (policy(@grant).grant_editor_access? && @grant.reviews.completed.none?)
     end
 
     def update
+      @grant = Grant.kept.friendly.find(params[:id])
+
       authorize @grant, :edit?
+
       if @grant.update(grant_params)
         flash[:notice] = 'Review form and criteria updated.'
         redirect_to criteria_grant_path(@grant)
@@ -22,10 +33,6 @@ module Grants
     end
 
     private
-
-    def set_grant
-      @grant = Grant.kept.friendly.find(params[:id])
-    end
 
     def grant_params
       params.require(:grant).permit(
--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -8,9 +8,15 @@ class Criterion < ApplicationRecord
                         Approach
                         Environment].freeze
 
-  belongs_to :grant,            inverse_of: :criteria
+  belongs_to :grant, inverse_of: :criteria
   has_many   :criteria_reviews, dependent: :destroy
 
   validates_presence_of :name
   validates :name, uniqueness: { scope: :grant }
+
+  validate :grant_may_not_have_completed_reviews, on: :create, if: -> () { grant.present? && grant.reviews.completed.any? }
+
+  def grant_may_not_have_completed_reviews
+    errors.add(:base, :may_not_change_criterion_after_reviews_submitted)
+  end
 end
