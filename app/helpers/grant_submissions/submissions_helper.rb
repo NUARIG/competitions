18888ce419f3cc75e595a61b64dd4f1c9cdde024
jseraf <john.d.serafin@gmail.com>
2023-10-04 15:47:50 -0500

    Add pluralizer helper for Applicant label

--- app/helpers/grant_submissions/submissions_helper.rb
+++ app/helpers/grant_submissions/submissions_helper.rb
@@ -1,16 +1,22 @@
-module GrantSubmissions::SubmissionsHelper
-  def review_data(submission)
-    if submission.reviews.any?
-      reviews = submission.reviews.to_a
-      OpenStruct.new(completed_review_count: reviews.count{ |review| !review.overall_impact_score.nil? },
-                     assigned_review_count:  reviews.count,
-                     overall_impact_average: submission.average_overall_impact_score,
-                     composite_score:        submission.composite_score )
-    else
-      OpenStruct.new(completed_review_count: '0',
-                     assigned_review_count:  '0',
-                     overall_impact_average: '-',
-                     composite_score:        '-')
+module GrantSubmissions
+  module SubmissionsHelper
+    def review_data(submission)
+      if submission.reviews.any?
+        reviews = submission.reviews.to_a
+        OpenStruct.new(completed_review_count: reviews.count{ |review| !review.overall_impact_score.nil? },
+                       assigned_review_count:  reviews.count,
+                       overall_impact_average: submission.average_overall_impact_score,
+                       composite_score:        submission.composite_score )
+      else
+        OpenStruct.new(completed_review_count: '0',
+                       assigned_review_count:  '0',
+                       overall_impact_average: '-',
+                       composite_score:        '-')
+      end
+    end
+
+    def applicant_label(applicants)
+      'Applicant'.pluralize(applicants.count) + ':'
     end
   end
 end
