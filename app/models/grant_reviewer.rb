5bb3e0972307f237674eadcc6fb3fe54833dba53
jseraf <john.d.serafin@gmail.com>
2024-04-26 16:17:41 -0500

    WIP: code cleanup

--- app/models/grant_reviewer.rb
+++ app/models/grant_reviewer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class GrantReviewer < ApplicationRecord
   has_paper_trail versions: { class_name: 'PaperTrail::GrantReviewerVersion' },
                   meta:     { grant_id: :grant_id , reviewer_id: :reviewer_id }
@@ -12,11 +10,4 @@ class GrantReviewer < ApplicationRecord
                         
   validates_presence_of   :grant, :reviewer
   validates_uniqueness_of :reviewer, scope: :grant
-
-  def can_be_assigned?(submission:)
-    return false if submission.grant != grant || submission.applicants.includes?(reviewer)
-    return false if submission.reviewers.includes?(reviewer)
-
-    reviewer.reviews.by_grant(grant).length < grant.max_submissions_per_reviewer
-  end
 end
