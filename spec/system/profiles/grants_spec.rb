cc29f62c979396c23e0b6c1bb028361cde702d66
jseraf <john.d.serafin@gmail.com>
2024-06-11 13:27:33 -0500

    Add specs for panel and permissions links

--- spec/system/profiles/grants_spec.rb
+++ spec/system/profiles/grants_spec.rb
@@ -3,17 +3,26 @@ include UsersHelper
 
 RSpec.describe 'Profile Grants', type: :system, js: true do
   let(:unaffiliated_grant)        { create(:grant) }
-  let(:grant1)                    { create(:open_grant_with_users_and_form_and_submission_and_reviewer, name: "First #{Faker::Lorem.sentence(word_count: 3)}") }
+  let(:grant1)                    do
+    create(:open_grant_with_users_and_form_and_submission_and_reviewer,
+           name: "First #{Faker::Lorem.sentence(word_count: 3)}")
+  end
   let(:grant2)                    { create(:draft_grant) }
   let(:grant3)                    { create(:published_grant) }
-  let(:grant4)                    { create(:draft_grant)}
-  let!(:grant1_permission)         { grant1.grant_permissions.role_admin.first }
-  let!(:grant2_viewer_permission)  { create(:viewer_grant_permission,  grant: grant2,
-                                                                      user: grant1.admins.first) }
-  let!(:grant3_editor_permission)  { create(:editor_grant_permission,  grant: grant3,
-                                                                      user: grant1.admins.first) }
-  let!(:grant4_admin_permission)   { create(:admin_grant_permission,   grant: grant4,
-                                                                      user: grant1.admins.first) }
+  let(:grant4)                    { create(:draft_grant) }
+  let!(:grant1_permission)        { grant1.grant_permissions.role_admin.first }
+  let!(:grant2_viewer_permission) do
+    create(:viewer_grant_permission,  grant: grant2,
+                                      user: grant1.admins.first)
+  end
+  let!(:grant3_editor_permission) do
+    create(:editor_grant_permission,  grant: grant3,
+                                      user: grant1.admins.first)
+  end
+  let!(:grant4_admin_permission) do
+    create(:admin_grant_permission,   grant: grant4,
+                                      user: grant1.admins.first)
+  end
 
   let(:grant_admin)               { grant1.admins.first }
   let(:user)                      { create(:saml_user) }
@@ -41,6 +50,7 @@ RSpec.describe 'Profile Grants', type: :system, js: true do
   context '#index' do
     context 'grant_admin' do
       before(:each) do
+        grant_admin.update(grant_creator: false)
         login_user grant_admin
         visit profile_grants_path
       end
@@ -66,8 +76,10 @@ RSpec.describe 'Profile Grants', type: :system, js: true do
             expect(page).to have_link 'Edit',         href: edit_grant_path(grant1)
             expect(page).to have_link 'Submissions',  href: grant_submissions_path(grant1)
             expect(page).to have_link 'Reviews',      href: grant_reviews_path(grant1)
-            expect(page).to have_link 'Duplicate',    href: new_grant_duplicate_path(grant1)
-            expect(page).not_to have_link 'Delete',   href: grant_path(grant1)
+            expect(page).to have_link 'Panel',        href: edit_grant_panel_path(grant1)
+            expect(page).to have_link 'Permissions',  href: grant_grant_permissions_path(grant1)
+            expect(page).not_to have_link 'Duplicate', href: new_grant_duplicate_path(grant1)
+            expect(page).not_to have_link 'Delete', href: grant_path(grant1)
 
             find("td.manage[data-grant-id='#{grant4.id}']").hover
             expect(page).to have_link 'Delete', href: grant_path(grant4)
@@ -80,30 +92,50 @@ RSpec.describe 'Profile Grants', type: :system, js: true do
             expect(page).to have_link 'Edit',           href: edit_grant_path(grant3)
             expect(page).to have_link 'Submissions',    href: grant_submissions_path(grant3)
             expect(page).to have_link 'Reviews',        href: grant_reviews_path(grant3)
-            expect(page).not_to have_link 'Duplicate',  href: new_grant_duplicate_path(grant2)
-            expect(page).not_to have_link 'Delete',     href: grant_path(grant2)
+            expect(page).to have_link 'Panel',          href: edit_grant_panel_path(grant3)
+            expect(page).to have_link 'Permissions',    href: grant_grant_permissions_path(grant3)
+            expect(page).not_to have_link 'Duplicate',  href: new_grant_duplicate_path(grant3)
+            expect(page).not_to have_link 'Delete',     href: grant_path(grant3)
           end
         end
 
         context 'viewer role' do
           scenario 'includes proper links' do
             find("td.manage[data-grant-id='#{grant2.id}']").hover
-            expect(page).to have_link 'Edit',           href: edit_grant_path(grant2)
-            expect(page).to have_link 'Submissions',    href: grant_submissions_path(grant2)
-            expect(page).to have_link 'Reviews',        href: grant_reviews_path(grant2)
-            expect(page).not_to have_link 'Duplicate',  href: new_grant_duplicate_path(grant2)
-            expect(page).not_to have_link 'Delete',     href: grant_path(grant2)
+            expect(page).to have_link 'Edit',             href: edit_grant_path(grant2)
+            expect(page).to have_link 'Submissions',      href: grant_submissions_path(grant2)
+            expect(page).to have_link 'Reviews',          href: grant_reviews_path(grant2)
+            expect(page).to have_link 'Panel',            href: grant_panel_path(grant2)
+            expect(page).not_to have_link 'Permissions',  href: grant_grant_permissions_path(grant2)
+            expect(page).not_to have_link 'Duplicate',    href: new_grant_duplicate_path(grant2)
+            expect(page).not_to have_link 'Delete',       href: grant_path(grant2)
           end
         end
       end
 
       scenario 'it can be filtered on grant name' do
-        find_field('Search Grant Name', with: '').set ('First')
+        find_field('Search Grant Name', with: '').set('First')
         click_button 'Search'
 
         expect(page).to have_link(grant1.name, href: grant_path(grant1))
         expect(page).not_to have_link(grant2.name, href: grant_path(grant2))
         expect(page).not_to have_link(grant3.name, href: grant_path(grant3))
+      end
+    end
+
+    context 'grant_creator and grant_admin' do
+      scenario 'includes proper links' do
+        login_user grant_admin
+        visit profile_grants_path
+
+        find("td.manage[data-grant-id='#{grant1.id}']").hover
+        expect(page).to have_link 'Edit',         href: edit_grant_path(grant1)
+        expect(page).to have_link 'Submissions',  href: grant_submissions_path(grant1)
+        expect(page).to have_link 'Reviews',      href: grant_reviews_path(grant1)
+        expect(page).to have_link 'Panel',        href: edit_grant_panel_path(grant1)
+        expect(page).to have_link 'Permissions',  href: grant_grant_permissions_path(grant1)
+        expect(page).to have_link 'Duplicate',    href: new_grant_duplicate_path(grant1)
+        expect(page).not_to have_link 'Delete',   href: grant_path(grant1)
       end
     end
   end
