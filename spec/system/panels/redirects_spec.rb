2821760e1ee2fbaefb614b9d62c928275b7cfd48
jseraf <john.d.serafin@gmail.com>
2024-07-27 11:06:01 -0500

    WIP: update and clarify panel redirect specs

--- spec/system/panels/redirects_spec.rb
+++ spec/system/panels/redirects_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Panels', type: :system, js: true do
+RSpec.describe 'PanelsRedirect', type: :system, js: true do
   let(:grant)       { create(:published_closed_grant_with_users) }
   let(:panel)       { grant.panel }
   let(:submitted_review) do
@@ -14,17 +14,28 @@ RSpec.describe 'Panels', type: :system, js: true do
   let(:user) { create(:saml_user) }
 
   describe 'Redirect' do
-    context 'unauthorized users' do
+    context 'panel page' do
       scenario 'submitter is redirected' do
         login_user submitter
         visit grant_panel_path(grant)
 
         expect(current_path).to eql(root_path)
       end
+    end
 
+    context 'submission page' do
       scenario 'unconnected user is redirected' do
         login_user user
         visit grant_panel_submission_path(grant, submission)
+
+        expect(current_path).to eql(root_path)
+      end
+    end
+
+    context 'submission reviews page' do
+      scenario 'unconnected user is redirected' do
+        login_user user
+        visit grant_panel_submission_reviews_path(grant, submission)
 
         expect(current_path).to eql(root_path)
       end
